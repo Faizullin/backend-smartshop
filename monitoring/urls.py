6817a43af4e2443efe6cd2718cf76b44f876ff43
Faizullin <osmanfajzulla@gmail.com>
2023-03-27 23:51:10 +0600

    save

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -46,15 +46,9 @@ def save_system_data():
         current_count = SystemData.objects.count()
         
         if current_count > MAX_SAVE_NUMBER:
-            #SystemData.objects.order_by('id')[:(current_count - MAX_SAVE_NUMBER)].delete()
             first_n_records = SystemData.objects.order_by('id').filter(id__lt = (system_data.pk - MAX_SAVE_NUMBER))
-
-# Delete the first 10 records
             first_n_records.delete()
             
-
-        # Wait for 1 second before getting the next set of data
-        #await asyncio.sleep(1)
         time.sleep(7)
 
 
